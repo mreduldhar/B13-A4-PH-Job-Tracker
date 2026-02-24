c002e0decfdf8abb709da752ef82ee49a96c486a
Mredul kanthy Dhar <mreduldhar237@gmail.com>
2026-02-24 19:35:38 +0600

    reject btn functionality added

--- js/script.js
+++ js/script.js
@@ -49,13 +49,14 @@ function toggleButton(id) {
   if (id == "interview-btn") {
     allCards.classList.add("hidden");
     filterSection.classList.remove("hidden");
-    renderThriving();
+    renderInterview();
   } else if (id == "all-btn") {
     allCards.classList.remove("hidden");
     filterSection.classList.add("hidden");
   } else if (id == "rejected-btn") {
     allCards.classList.add("hidden");
     filterSection.classList.remove("hidden");
+    renderRejected();
   }
 }
 
@@ -97,9 +98,62 @@ mainContainer.addEventListener("click", function (event) {
     if (!headerExist) {
       interviewList.push(cardInfo);
     }
-    //   console.log("Interview List", interviewList);
 
+    // remove the interview from rejected list
+    const rejectedList = rejectedList.filter(function (item) {
+      return item.cardHeader != cardInfo.cardHeader;
+    });
+
+    if (currentStatus == "rejected-btn") {
+      renderRejected();
+    }
+
+    calculateCount();
     renderInterview();
+  } else if (event.target.classList.contains("rejected-btn")) {
+    const parentNode = event.target.parentNode.parentNode.parentNode;
+
+    const cardHeader = parentNode.querySelector(".card-header").innerText;
+    const cardSubHeader =
+      parentNode.querySelector(".card-sub-header").innerText;
+    const jobStatus = parentNode.querySelector(".job-status").innerText;
+    const cardBadge = parentNode.querySelector(".card-badge").innerText;
+    const badgeDesc = parentNode.querySelector(".badge-desc").innerText;
+    const interviewBtn = parentNode.querySelector(".interview-btn").innerText;
+    const rejectedBtn = parentNode.querySelector(".rejected-btn").innerText;
+
+    parentNode.querySelector(".card-badge").innerText = "Rejected";
+
+    // create object
+    cardInfo = {
+      cardHeader,
+      cardSubHeader,
+      jobStatus,
+      cardBadge: "Rejected",
+      badgeDesc,
+      interviewBtn,
+      rejectedBtn,
+    };
+
+    const headerExist = rejectedList.find(function (item) {
+      return item.cardHeader == cardInfo.cardHeader;
+    });
+
+    if (!headerExist) {
+      rejectedList.push(cardInfo);
+    }
+
+    // remove the interview from rejected list
+    const interviewList = interviewList.filter(function (item) {
+      return item.cardHeader != cardInfo.cardHeader;
+    });
+
+    if (currentStatus == "interview-btn") {
+      renderInterview();
+    }
+
+    calculateCount();
+    renderRejected();
   }
 });
 
@@ -135,6 +189,67 @@ function renderInterview() {
               >
               <p class="badge-desc text-sm md:text-[16px] text-[#323B49] mt-3">
                 ${interview.badgeDesc}
+              </p>
+            </div>
+
+            <!-- Buttons -->
+            <div class="buttons flex gap-2">
+              <button
+                class="interview-btn btn border border-[#10B981] text-[#10B981] hover:bg-[#10B981] hover:text-white"
+              >
+                Interview
+              </button>
+              <button
+                class="rejected-btn btn border border-[#EF4444] text-[#EF4444] hover:bg-[#EF4444] hover:text-white"
+              >
+                Rejected
+              </button>
+            </div>
+          </div>
+          <div>
+            <button
+              id=""
+              class="delete-btn btn w-8 h-8 border border-[#F1F2F4] bg-transparent p-2 rounded-full"
+            >
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+          </div>`;
+    filterSection.appendChild(div);
+  }
+}
+
+// render rejected section
+function renderRejected() {
+  filterSection.innerHTML = "";
+
+  // create card element
+  for (let reject of rejectedList) {
+    // console.log(reject);
+
+    let div = document.createElement("div");
+    div.className = "bg-white p-6 flex justify-between rounded-xl";
+    div.innerHTML = ` <div class="space-y-5">
+            <!-- card heading -->
+            <div>
+              <h3 class="card-header text-[#002C5C] text-xl font-medium">
+                ${reject.cardHeader}
+              </h3>
+              <p class="card-sub-header text-[#64748B]">
+                ${reject.cardSubHeader}
+              </p>
+            </div>
+
+            <p class="job-status text-sm md:text-[16px] text-[#64748B]">
+             ${reject.jobStatus}
+            </p>
+
+            <div>
+              <span
+                class="card-badge text-sm md:text-[16px] text-[#002C5C] font-medium bg-[#EEF4FF] py-2 px-3 rounded-md uppercase"
+                >${reject.cardBadge}</span
+              >
+              <p class="badge-desc text-sm md:text-[16px] text-[#323B49] mt-3">
+                ${reject.badgeDesc}
               </p>
             </div>
 
